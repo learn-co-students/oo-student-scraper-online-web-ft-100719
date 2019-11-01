6d1f1aeab002c5a73757cd2420912ce7f2264303
joshua richards <jermain119@gmail.com>
2019-11-01 22:40:29 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
+  def self.scrape_index_page(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html)
     
   end
 
